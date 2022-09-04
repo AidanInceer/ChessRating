2304d54f88c609c866f3380fe7d2046c3cd4c0c4
AidanInceer <aidaninceer0@gmail.com>
2022-09-04 21:41:43 +0100

    extract part 2

--- chessrating/extract.py
+++ chessrating/extract.py
@@ -1,28 +1,32 @@
+from dataclasses import dataclass
 import chessdotcom
 import json
 import pandas as pd
 import requests
 
+@dataclass
+class Extract:
+    username: str
 
-def run_data_extract(username: str) -> None:
-    urls = chessdotcom.get_player_game_archives(username).json
-    username_list = []
-    games_list = []
+    def run_data_extract(self: str) -> None:
+        urls = chessdotcom.get_player_game_archives(self.username).json
+        username_list = []
+        games_list = []
 
-    for url in urls["archives"]:
-        data = requests.get(url).json()
+        for url in urls["archives"]:
+            data = requests.get(url).json()
 
-        for game_pgn in data["games"]:
-            # chess_game_string = str(game_pgn["pgn"]).replace("\n", " ; ")
-            games_list.append(game_pgn["pgn"])
-            username_list.append(username)
+            for game_pgn in data["games"]:
+                # chess_game_string = str(game_pgn["pgn"]).replace("\n", " ; ")
+                games_list.append(game_pgn["pgn"])
+                username_list.append(self.username)
 
-    game_dict = {
-        "username": username_list,
-        "game_data": games_list
-    }
-    pgn_df = pd.DataFrame(game_dict)
-    print(pgn_df["game_data"])
+        game_dict = {
+            "username": username_list,
+            "game_data": games_list
+        }
+        pgn_df = pd.DataFrame(game_dict)
+        print(pgn_df["game_data"])
 
  
-run_data_extract("Ainceer")
+Extract.run_data_extract("Ainceer")
